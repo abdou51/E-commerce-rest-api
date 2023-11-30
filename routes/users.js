11b0f3d4388e4bcea9ef0fff0a48bac882e775fe
abdou51 <bgh.prods@gmail.com>
2023-11-30 22:58:18 +0100

    added login

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.post("/login", async (req, res) => {
         type: user.type,
       },
       secret,
-      { expiresIn: null }
+      { expiresIn: 60 * 60 * 24 * 365 * 100 }
     );
 
     res.status(200).json({
@@ -29,6 +29,39 @@ router.post("/login", async (req, res) => {
     });
   } else {
     res.status(400).send("password is wrong!");
+  }
+});
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password, type } = req.body;
+    console.log(req.body);
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists",
+      });
+    }
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+      username,
+      passwordHash,
+      type,
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: savedUser,
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
   }
 });
 
